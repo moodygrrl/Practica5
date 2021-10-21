178b0b881010c2036a12f503672ca4e5cbd07d91
moodygrrl <rebeca.criado@educa.madrid.org>
2021-10-21 16:23:55 +0200

    Practica5

---
+++ Ejercicio1.java
@@ -0,0 +1,21 @@
+package variablesDatosOperadores;
+        /*
+        Modifica el siguiente programa para hacer que compile y funcione:
+
+        public class Suma {
+         public static void main(String[] args) {
+          int n1 = 50, n2 = 30
+          boolean suma = 0;
+          suma = n1 * n2;
+          System.out.println("La suma es: " + suma);
+
+         */
+public class Ejercicio1 {
+    public static void main(String[] args) {
+        int n1 = 50, n2 = 30;
+        int suma = n1 + n2;
+        int producto = n1 * n2;
+        System.out.println("La suma es: " + suma);
+        System.out.println("La multiplicación es: " + producto);
+    }
+}
